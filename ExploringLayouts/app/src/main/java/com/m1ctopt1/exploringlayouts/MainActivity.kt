63393cc9a828838590d5b5809d7916836b74aa53
Tabitha Anderson <tabitha.anderson@csuglobal.edu>
2024-10-14 12:32:16 -0700

    Adding all three types of layouts

---
+++ ExploringLayouts/app/src/main/java/com/m1ctopt1/exploringlayouts/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.m1ctopt1.exploringlayouts
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+            setContentView(R.layout.main_menu)
+    }
+    fun loadConstraintLayout(v: View) {
+        setContentView(R.layout.activity_main)
+    }
+    fun loadTableLayout(v: View) {
+        //setContentView(R.layout.my_table_layout)
+    }
+    fun loadMenuLayout(v: View) {
+        setContentView(R.layout.main_menu)
+    }
+}
