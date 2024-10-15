00f915105e4ec3344816ca50ba3fa2378bf724c5
Tabitha Anderson <tabitha.anderson@csuglobal.edu>
2024-10-14 22:03:55 -0700

    Finished Chp 5 Activity Scroll

---
+++ CardViewLayout/app/src/main/java/com/m1ctopt1/cardviewlayout/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.m1ctopt1.cardviewlayout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.my_layout)
+
+    }
+}
