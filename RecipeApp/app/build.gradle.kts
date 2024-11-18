632386b99ab1a5fb8cad1fbe7b965f4c61641207
Tabitha Anderson <tabitha.anderson@csuglobal.edu>
2024-11-17 23:50:53 -0800

    Got background of app completed

---
+++ RecipeApp/app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+   id("com.google.devtools.ksp")
+
+
+
+}
+
+android {
+    namespace = "com.m1ctopt1.recipeapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.m1ctopt1.recipeapp"
+        minSdk = 22
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+
+    //circle image view
+        implementation(libs.circleimageview)
+    //scalable unit text size
+        implementation("com.intuit.ssp:ssp-android:1.1.1")
+    //scalable unit size
+        implementation ("com.intuit.sdp:sdp-android:1.1.1")
+    //room database
+        val room_version = "2.6.1"
+        implementation("androidx.room:room-runtime:$room_version")
+        ksp("androidx.room:room-compiler:$room_version")
+        implementation("androidx.room:room-ktx:$room_version")
+        testImplementation("androidx.room:room-testing:$room_version")
+        implementation(libs.roundedimageview)
+    //crop image library
+        implementation("com.vanniktech:android-image-cropper:4.6.0")
+    //easy permission
+        implementation(libs.easypermissions)
+    //coroutines core
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+
+
+
+}
